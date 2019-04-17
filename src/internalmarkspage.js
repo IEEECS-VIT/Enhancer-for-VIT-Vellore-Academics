4712152bfa3c2cba0535d1ee5e28e663c59de948
tarush <tarush@vicara.co>
2019-04-17 18:07:08 +0530

    Added viewing of Total Weightage Marks:

--- src/internalmarkspage.js
+++ src/internalmarkspage.js
@@ -29,6 +29,52 @@ const modifyInternalMarksPage = () => {
 
     }).get();
 
+    // jQuery('.customTable-level1 > tbody').map(function() {        
+    //     console.log( jQuery('.tableContent-level1') );
+    //     this.innerHTML += "<tr> Lmao </tr>"
+    // }).get();
+
+    jQuery('.customTable-level1 > tbody').map(function() {
+        
+        var totalClassWeightage = 0, totalUserWeightage = 0, totalMaxMark = 0, totalWeightage = 0, totalScoredMark = 0, totalClassAverage = 0;
+
+        jQuery(this).find(".tableContent-level1").map(function() {
+            console.log(this);
+            
+            var tableContent = this.innerHTML.split('<td>');
+
+            var maxMark = tableContent[3].replace( /[^\d.]/g, '' );
+            var weightage = tableContent[4].replace( /[^\d.]/g, '' );
+            var scoredMark = tableContent[6].replace( /[^\d.]/g, '' );
+            var userWeightage = tableContent[7].replace( /[^\d.]/g, '' );
+            var classAverage = tableContent[8].replace( /[^\d.]/g, '' );
+            var classWeightage = tableContent[9].replace( /[^\d.]/g, '' );
+            
+            totalMaxMark += parseFloat(maxMark);
+            totalWeightage += parseFloat(weightage);
+            totalScoredMark += parseFloat(scoredMark);
+            totalClassAverage += parseFloat(classAverage);
+            totalUserWeightage += parseFloat(userWeightage);
+            totalClassWeightage += parseFloat(classWeightage);
+        })       
+        
+        this.innerHTML += `
+        <tr class='tableContent-level1' style='background: #efd2a5;' >
+            <td></td>
+            <td><b>Total:</b></td>
+            <td>` + totalMaxMark.toFixed(2) + `</td>
+            <td>` + totalWeightage.toFixed(2) + `</td>
+            <td></td>
+            <td>` + totalScoredMark.toFixed(2) + `</td>
+            <td style='font-weight: 700' >` + totalUserWeightage.toFixed(2) + `</td>
+            <td>` + totalClassAverage.toFixed(2) + `</td>
+            <td style='font-weight: 700' >` + totalClassWeightage.toFixed(2) + `</td>
+            <td></td>
+            <td></td>
+        </tr>`;
+
+    }).get();
+
     jQuery.unblockUI();
 };
 
