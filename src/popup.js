a729c77624f1d5af989a213fb8b568f850b23793
Rishabh Agrawal <83543522+Rishabhco@users.noreply.github.com>
2022-02-23 12:19:33 +0530

    added shortcuts

--- src/popup.js
+++ src/popup.js
@@ -1,18 +1,126 @@
 // Once the DOM is ready...
+
 window.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("themeSet").onclick = function () {
-    for (let i of document.getElementsByName("themeSelect")) {
-      if (i.checked) {
-        chrome.tabs.query(
-          { active: true, currentWindow: true },
-          function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {
-              message: "style:" + i.value,
-            });
-          }
-        );
-        break;
-      }
-    }
+  // document.getElementById("themeSet").onclick = function () {
+  //   for (let i of document.getElementsByName("themeSelect")) {
+  //     if (i.checked) {
+  //       chrome.tabs.query(
+  //         { active: true, currentWindow: true },
+  //         function (tabs) {
+  //           chrome.tabs.sendMessage(tabs[0].id, {
+  //             message: "style:" + i.value,
+  //           });
+  //         }
+  //       );
+  //       break;
+  //     }
+  //   }
+  // };
+
+  document.getElementById("openDigitalAssignmentUpload").onclick = function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        message: "openDigitalAssignmentUpload",
+      });
+    });
   };
+
+  document.getElementById("openCoursePage").onclick = function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        message: "openCoursePage",
+      });
+    });
+  };
+
+
+  document.getElementById("openExamSchedule").onclick = function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        message: "openExamSchedule",
+      });
+    });
+  };
+
+  document.getElementById("openMarksView").onclick = function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        message: "openMarksView",
+      });
+    });
+  };
+
+  document.getElementById("openAcademicCalendar").onclick = function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        message: "openAcademicCalendar",
+      });
+    });
+  };
+
+  document.getElementById("openClassAttendance").onclick = function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        message: "openClassAttendance",
+      });
+    });
+  };
+
+  document.getElementById("openTimeTable").onclick = function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        message: "openTimeTable",
+      });
+    });
+  };
+
+  document.getElementById("openEventRegistration").onclick = function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        message: "openEventRegistration",
+      });
+    });
+  };
+
+  let flag = true;
+
+  document.getElementById("moreactions").addEventListener("click", function () {
+    if (flag) {
+      document.getElementsByClassName("none")[0].style.display = "block";
+      document.getElementsByClassName("none")[1].style.display = "block";
+      document.getElementsByClassName("none")[2].style.display = "block";
+      document.getElementsByClassName("none")[3].style.display = "block";
+      document.getElementById("content").style.height = "587px";
+      document.getElementsByClassName("last")[0].style.marginBottom = "0px";
+      document.getElementById("downarrow").style.display = "none";
+      document.getElementById("uparrow").style.display = "inline";
+      flag = false;
+    }
+    else {
+      document.getElementsByClassName("none")[0].style.display = "none";
+      document.getElementsByClassName("none")[1].style.display = "none";
+      document.getElementsByClassName("none")[2].style.display = "none";
+      document.getElementsByClassName("none")[3].style.display = "none";
+      document.getElementById("content").style.height = "360px";
+      document.getElementsByClassName("last")[0].style.marginBottom = "12.5px";
+      document.getElementById("downarrow").style.display = "inline";
+      document.getElementById("uparrow").style.display = "none";
+      flag = true;
+    }
+  });
+
+  let modeFlag = true;
+
+  document.getElementById("mode").addEventListener("click", function () {
+    if (modeFlag) {
+      document.getElementById("modeText").innerHTML = "Light Mode";
+      document.getElementsByTagName("link")[3].setAttribute("href", "styles/general/darkMode.css");
+      modeFlag = false;
+    }
+    else {
+      document.getElementById("modeText").innerHTML = "Dark Mode";
+      document.getElementsByTagName("link")[3].setAttribute("href", "styles/general/lightMode.css");
+      modeFlag = true;
+    }
+  })
 });
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,59 @@
 chrome.runtime.onMessage.addListener((request) => {
   console.log(
     "Contentscript has received a message from from background script: '" +
-      request.message +
-      "'"
+    request.message +
+    "'"
   );
+
+  if (request.message == "openCoursePage") {
+    const button = document.createElement("button");
+    button.setAttribute(
+      "onclick",
+      "loadmydiv('academics/common/StudentCoursePage')"
+    );
+    button.click();
+  }
+
+  if (request.message == "openDigitalAssignmentUpload") {
+    const button = document.createElement("button");
+    button.setAttribute("onclick", "loadmydiv('examinations/StudentDA')");
+    button.click();
+  }
+
+  if (request.message == "openExamSchedule") {
+    const button = document.createElement("button");
+    button.setAttribute("onclick", "loadmydiv('examinations/StudExamSchedule')");
+    button.click();
+  }
+
+  if (request.message == "openMarksView") {
+    const button = document.createElement("button");
+    button.setAttribute("onclick", "loadmydiv('examinations/StudentMarkView')");
+    button.click();
+  }
+
+  if (request.message == "openAcademicCalendar") {
+    const button = document.createElement("button");
+    button.setAttribute("onclick", "loadmydiv('academics/common/CalendarPreview')");
+    button.click();
+  }
+
+  if (request.message == "openClassAttendance") {
+    const button = document.createElement("button");
+    button.setAttribute("onclick", "loadmydiv('academics/common/StudentAttendance')");
+    button.click();
+  }
+
+  if (request.message == "openTimeTable") {
+    const button = document.createElement("button");
+    button.setAttribute("onclick", "loadmydiv('academics/common/StudentTimeTable')");
+    button.click();
+  }
+
+  if (request.message == "openEventRegistration") {
+    const button = document.createElement("button");
+    button.setAttribute("onclick", "loadmydiv('event/swf/loadEventRegistration')");
+    button.click();
+  }
+
 });
